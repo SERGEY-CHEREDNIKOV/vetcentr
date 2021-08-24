5be4d95a5974a86a1b451daec4daecbe43e11374
Sergey <serezha.ros@mail.ru>
2021-08-24 14:57:22 +0300

    add block slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,27 @@
+import Swiper from 'swiper/bundle';
 import a from "./modules/test";
 
-window.addEventListener('load', function(){ 
+window.addEventListener('load', function () {
 
     'use strict';
+
+    const swiper = new Swiper('.slider__list', {
+        touchRatio: 2,
+        grabCursor: true,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+        loop: true,
+        // autoplay: {
+        //     delay: 3000,
+        //     stopOnLastSlide: false,
+        //     disableOnInteraction: false,
+        // },
+        speed: 1000,
+        parallax: true,
+    });
 
     a();
 
