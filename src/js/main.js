83aa3d0a55698915550bb39663824655f735d91a
Sergey <serezha.ros@mail.ru>
2021-08-31 15:22:40 +0300

    add about

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import Swiper from 'swiper/bundle';
-
 import WOW from 'wow.js/dist/wow.js';
 import toSwitchTabs from "./modules/tabs";
 import a from "./modules/test";
@@ -17,18 +16,43 @@ window.addEventListener('load', function () {
             pageUpDown: true,
         },
         loop: true,
-        // autoplay: {
-        //     delay: 3000,
-        //     stopOnLastSlide: false,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 3000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false,
+        },
         speed: 1000,
         parallax: true,
     });
 
-    toSwitchTabs('.tabs__triggers-item', '.tabs__content-item', 'tabs__triggers-item--active', 'tabs__content-item--active');
+    new Swiper('.about__slider', {
+        navigation: {
+            nextEl: '.about__button-prev',
+            prevEl: '.about__button-next',
+        },
+        touchRatio: 2,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        autoplay: {
+            delay: 3000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false,
+        },
+        speed: 1000,
+        loop: true,
+        autoHeight: true,
+    });
 
     new WOW().init();
+
+    toSwitchTabs('.tabs__triggers-item', '.tabs__content-item', 'tabs__triggers-item--active', 'tabs__content-item--active');
 
     a();
 
